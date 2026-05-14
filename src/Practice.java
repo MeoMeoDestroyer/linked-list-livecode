0abae14e03fe0440e0d1a348021c668557688edc
Kate_nguyen <253353253+MeoMeoDestroyer@users.noreply.github.com>
2026-05-14 11:21:33 -0700

    in class

--- src/Practice.java
+++ src/Practice.java
@@ -5,20 +5,73 @@ public class Practice {
         Node otherNode = new Node('x', null);
 
         Node coolNode = new Node('e', null);
+        Node helloNode = new Node('f', null);
+
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+        coolNode.next = helloNode;
         
         myNode.next = otherNode;
         otherNode.next = coolNode;
 
+        Node newHead = removeAt(myNode, 2);
+        Node current = newHead;
 
-        Node current = myNode;
         while (current != null){
             System.out.println(current.value);
             current = current.next;
-    }
+        }
+
+        // int numNodes = length(myNode);
+        // System.out.println(numNodes);
+
+    //     Node current = myNode;
+    //     while (current != null){
+    //         System.out.println(current.value);
+    //         current = current.next;
+    // }
 
 
         // System.out.println(myNode.value);
         // System.out.println(otherNode.value);
         // System.out.println(coolNode.value);
+    }
+
+    // count how many nodes are in the linked list
+    public static int length (Node head) {
+        int count  = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+
+    // remove the node at removeIdx
+    // head is at index 0
+    // example w->x->e->f
+    // removeIdx 2
+    // result w->x->f
+    // return the new head of the list
+    public static Node removeAt( Node head, int removeIdx){
+        if (removeIdx < 0){
+            throw new IndexOutOfBoundsException("removeIndx must be >= 0");
+        }
+        if (removeIdx ==0){
+            return head.next;
+        }
+        Node current = head;
+        
+
+        for (int i = 0; i < removeIdx - 1;i++) {
+            if (current == null){
+                throw new IndexOutOfBoundsException("removeIdx must be < len(list");
+            }
+            current = current.next;
+        }
+        current.next= current.next.next;
+        return head;
     }
 }
